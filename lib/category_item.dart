58ebef409bc8adb97f6e750803fda31b3d13cf18
Yugal Panchal <yugalpanchal01@gmail.com>
2021-11-07 00:21:30 +0530

    new screen connected

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String text;
@@ -9,21 +10,39 @@ class CategoryItem extends StatelessWidget {
     this.color,
   });
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(text),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      splashColor: Theme.of(context).accentColor,
+      onTap: () => selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          text,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
